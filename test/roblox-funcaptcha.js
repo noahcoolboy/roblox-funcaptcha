70db987190e7656974d6a9e87f749eedad5daebe
noahcoolboy <46800081+noahcoolboy@users.noreply.github.com>
2022-02-21 22:27:48 +0100

    Test each public key

--- test/roblox-funcaptcha.js
+++ test/roblox-funcaptcha.js
@@ -4,7 +4,9 @@ function isValidImage(image) {
     return image.toString().startsWith("GIF8")
 }
 
-fun.getToken("476068BF-9607-4799-B53D-966BE98E2B81").then(async token => {
+async function test(publicKey, name) {
+    console.log("Testing key for " + name)
+    let token = await fun.getToken(publicKey)
     if(!token) {
         throw new Error("Invalid token")
     }
@@ -33,5 +35,13 @@ fun.getToken("476068BF-9607-4799-B53D-966BE98E2B81").then(async token => {
             throw new Error("Invalid image")
         }
     }
-    console.log("Test passed!")
+    console.log("Test passed for " + name + "!")
+})
+}
+
+setImmediate(async () => {
+    await test("476068BF-9607-4799-B53D-966BE98E2B81", "Login")
+    await test("476068BF-9607-4799-B53D-966BE98E2B81", "Signup")
+    await test("63E4117F-E727-42B4-6DAA-C8448E9B137F", "Group Join")
+    await test("1B154715-ACB4-2706-19ED-0DC7E3F7D855", "Promocode Redeem")
 })
